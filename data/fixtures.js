9c899b1de535c44e07c54d66f8c6c6ece95cdbb7
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-05-29 10:58:10 -0600

    initial commit

---
+++ data/fixtures.js
@@ -0,0 +1,18 @@
+module.exports = {
+  holes: [{
+    id: 1,
+    type: 'Yeast Doughnut Holes'
+  },
+  {
+    id: 2,
+    type: 'Cake Doughnut Holes'
+  },
+  {
+    id: 3,
+    type: 'Glazed Doughnut Holes'
+  },
+  {
+    id: 4,
+    type: 'Filled Doughnut Holes'
+  }
+  ]}
